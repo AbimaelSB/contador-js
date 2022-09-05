a2d2c729ec90fc182c5e4c7eb9670cc831ac0cf0
AbimaelSB <abimael.barros@yahoo.com>
2022-09-05 16:25:56 -0300

    upload files

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,40 @@
-var numero = parseInt(document.getElementById("nAtual").innerText);
+var cont = document.getElementById("nAtual");
+var numero = parseInt(cont.innerText);
+var botoes = document.getElementsByTagName('button');
 
-function subtrair(){
+console.log(cont);
+console.log(numero);
+
+function subtrair() {
   if(numero>-10){
     numero--;
-    document.getElementById('nAtual').innerHTML = numero;
+    cont.innerHTML = numero;
   }else{
     alert('-10 é o valor mínimo!');
   }
 }
 
-function somar(){
+function somar() {
   if(numero<10){
     numero++;
-    document.getElementById('nAtual').innerHTML = numero;
+    cont.innerHTML = numero;
   }else{
     alert('10 é o valor máximo!');
   }
 }
+
+function mudarCor() {
+  if(numero<0){
+    cont.style.color = 'red';
+  }else{
+    cont.style.color = 'black';
+  }
+}
+
+for (let i = 0; i < botoes.length; i++) {
+  botoes[i].addEventListener('click', mudarCor);
+}
+
+/*
+document.getElementsByTagName('button')[0].addEventListener('click',mudarCor);
+document.getElementsByTagName('button')[1].addEventListener('click',mudarCor);*/
